3c0921f6b7e278359b60bf27e694890544f7ce2a
focke-wolf <161486084+focke-wolf@users.noreply.github.com>
2024-03-07 15:45:32 +0200

    Add files via upload

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,7 @@
-  
+const hamburger = document.querySelector('.hamburger')
+const headerNav = document.querySelector('.header-nav')
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('is-active')
+    headerNav.classList.toggle('show')
+})
